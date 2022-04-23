b97afa784dd189aeb27145387bba43457905a9a3
Minh Nguyen <nqminhuit@gmail.com>
2022-04-23 14:39:51 +0700

    -

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((java-mode . (
+((nil . (
    (compile-command . "mvn -f ~/projects/gis/pom.xml clean package")
    (projectile-project-compilation-cmd . "mvn -f ~/projects/gis/pom.xml clean package")
-   (projectile-project-run-cmd . "cd ~/projects/small-git-root-module/; java -jar ~/projects/gis/target/gis-1.0.0.jar ")
+   (projectile-project-run-cmd . "cd ~/projects/small-git-root-module/; java -jar ~/projects/gis/target/gis-1.0.1.jar ")
 )))
