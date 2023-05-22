156a8651c4fff934bb104d724e87cda4b7a8a8b1
Minh Quoc Nguyen <minh.q.nguyen@opswat.com>
2023-05-22 17:13:10 +0700

    update emacs config for project

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . (
-   (compile-command . "mvn -f ~/projects/gis/pom.xml clean package")
-   (projectile-project-compilation-cmd . "mvn -f ~/projects/gis/pom.xml clean package")
-   (projectile-project-run-cmd . "cd ~/projects/small-git-root-module/; java -jar ~/projects/gis/target/gis-1.0.1.jar ")
+   (compile-command . "mvn -f /opt/minh/tools/gis/pom.xml clean package")
+   (projectile-project-compilation-cmd . "mvn -f /opt/minh/tools/gis/pom.xml clean package")
+   (projectile-project-run-cmd . "cd ~/projects/ocm-system/; java -jar /opt/minh/tools/gis/target/gis-1.1.2.jar ")
 )))
