0c6ed79efe07ddd7f6eeab7b789bdeae15d5cdc4
minh <minh.q.nguyen@opswat.com>
2024-07-17 08:36:02 +0700

    dev

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . (
-   (compile-command . "mvn -f ~/projects/gis/pom.xml -T 1C clean package")
-   (projectile-project-compilation-cmd . "mvn -T 1C clean package")
+   (compile-command . "mvn -f ~/projects/gis/pom.xml -T 1C clean package -DskipTests; cd /home/minh/projects/test/small-git-root-module; java -jar ~/projects/gis/target/gis-2.0.0-dev.jar ")
+   (projectile-project-compilation-cmd . "mvn -T 1C clean package -DskipTests")
    (projectile-project-run-cmd . "cd /home/minh/projects/test/small-git-root-module; java -jar ~/projects/gis/target/gis-2.0.0-dev.jar ")
 )))
