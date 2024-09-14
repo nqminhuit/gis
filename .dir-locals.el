5b84b9ed0ee6ddef087a4bfbc3ab3f88bd563739
minh <nqminhuit@gmail.com>
2024-09-14 16:49:06 +0700

    feat(dev): coding setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,6 @@
 
 ((nil . (
    (compile-command . "mvn -f /opt/minh/tools/gis/pom.xml -T 1C clean package -DskipTests; cd /home/minh/projects/test/small-git-root-module; java -jar /opt/minh/tools/gis/target/gis-2.0.0-dev.jar ")
-   (projectile-project-compilation-cmd . "mvn -T 1C clean package -DskipTests")
+   (projectile-project-compilation-cmd . "mvn -T 1C -DskipTests clean package")
    (projectile-project-run-cmd . "cd /home/minh/projects/test/small-git-root-module; java -jar /opt/minh/tools/gis/target/gis-2.0.0-dev.jar ")
 )))
